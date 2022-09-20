afbe8e6c754c3009efafb07fc41ff6636b16af31
Julien Biezemans <jb@jbpros.com>
2022-09-20 17:05:19 +0200

    Use azerty layout in autopairs completion keys

--- lua/jbpros/autopairs.lua
+++ lua/jbpros/autopairs.lua
@@ -14,11 +14,11 @@ npairs.setup {
   disable_filetype = { "TelescopePrompt", "spectre_panel" },
   fast_wrap = {
     map = "<M-e>",
-    chars = { "{", "[", "(", '"', "'" },
+    chars = { "{", "[", "(", '"', "'", "`" },
     pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
     offset = 0, -- Offset from pattern match
     end_key = "$",
-    keys = "qwertyuiopzxcvbnmasdfghjkl",
+    keys = "azertyuiopqsdfghjklmwxcvbn",
     check_comma = true,
     highlight = "PmenuSel",
     highlight_grey = "LineNr",
